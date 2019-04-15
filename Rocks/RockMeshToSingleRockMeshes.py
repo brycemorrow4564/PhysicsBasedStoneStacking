38cf5c71957f8386531beac11ae920348ed95dfe
Bryce Morrow <brycemorrow4564@gmail.com>
2019-04-15 12:24:17 -0400

    first commit

---
+++ Rocks/RockMeshToSingleRockMeshes.py
@@ -0,0 +1,19 @@
+import pymesh 
+import os 
+
+base_path = '../write_rocks'
+full_rock_mesh_path = f'{base_path}/rocks.obj'
+write_single_rocks_path = f'{base_path}/SingleRockFiles'
+
+full_rock_mesh = pymesh.load_mesh(full_rock_mesh_path)
+rock_meshes = pymesh.separate_mesh(full_rock_mesh)
+
+rockIndex = 1 
+for rock_mesh in rock_meshes: 
+    rock_mesh_path = f"{write_single_rocks_path}/rock{rockIndex}.obj"
+    pymesh.save_mesh(rock_mesh_path, rock_mesh)
+    print(f"Wrote rock {rockIndex}")
+    rockIndex += 1
+
+print("Finished!")
+
---
+++ docker-pymesh.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker run -i -t --mount type=bind,src=/Users/ALEX/Documents/RoboticsFinal/Rocks,dst=/write_rocks pymesh/pymesh
